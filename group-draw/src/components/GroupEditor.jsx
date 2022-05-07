e2f792c3c2f849653a05d5e36cd42bad1dc6ed7b
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-05-08 02:27:39 +0900

    update : 대륙 변경 또는 팀 추가 시 state 최신화

--- group-draw/src/components/GroupEditor.jsx
+++ group-draw/src/components/GroupEditor.jsx
@@ -8,10 +8,10 @@ const GroupEditor = function ({ drawList, insertTeam }) {
 
     const [selectedContinent, setContinent] = useState('uefa');
     const [selectedTeam, setTeam] = useState('esp');
-    const [selectedPort, setPort] = useState('1');
+    const [selectedPort, setPort] = useState('first');
 
     //mounted
-    useEffect(function () {}, []);
+    useEffect(function () {});
 
     /**
      * 대륙 변경
@@ -19,6 +19,20 @@ const GroupEditor = function ({ drawList, insertTeam }) {
      */
     const changeContinent = function (value) {
         setContinent(value);
+        setTeam(getContinentFisrtTeam(teamList, value));
+    };
+
+    /**
+     * 대륙별 첫 국가 얻어오기
+     * @param {array} teamList : 대륙리스트
+     * @param {string} continentCode : 대륙코드
+     */
+    const getContinentFisrtTeam = function (teamList, continentCode) {
+        const team = teamList.filter(function (item) {
+            return item.continent === continentCode;
+        });
+
+        return team.length ? team[0].code : '';
     };
 
     /**
@@ -41,15 +55,26 @@ const GroupEditor = function ({ drawList, insertTeam }) {
      * 팀 추가
      */
     const submit = function () {
-        let teamInfo = {};
-        teams.forEach(function (item) {
-            if (item.code === selectedTeam) {
-                teamInfo = item;
-                return false;
-            }
-        });
+        let targetTeam = {};
 
-        insertTeam({ ...teamInfo, selectedPort });
+        if (selectedTeam.length) {
+            teams.forEach(function (item) {
+                if (item.code === selectedTeam) {
+                    targetTeam = item;
+                    return false;
+                }
+            });
+
+            const availableTeams = teamList.filter(function (item) {
+                return item.code !== targetTeam.code;
+            });
+
+            insertTeam({ ...targetTeam, selectedPort });
+            setTeamList(availableTeams);
+            setTeam(getContinentFisrtTeam(availableTeams, selectedContinent));
+        } else {
+            alert('국가를 선택해주세요..');
+        }
     };
 
     return (
